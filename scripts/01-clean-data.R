301fe8588cd82a0df233d3bd157c11da8baf573c
Quinn Webber <46348512+qwebber@users.noreply.github.com>
2023-03-22 11:29:39 -0400

    Update 01-clean-data.R

--- scripts/01-clean-data.R
+++ scripts/01-clean-data.R
@@ -1,3 +1,22 @@
 
 
+## load packages
+ library(data.table)
+ library(tidyverse)
+ library(ggplot2)
 
+## load data
+age <- read_csv("input/age-parasitism.csv")
+ 
+## header
+head(age)
+ 
+## number of unique studies
+unique(age$filename)
+
+## number of relationships by host 
+age %>%
+  group_by(host_taxonomy) %>%
+  summarise(host_taxonomy = n())
+  
+  
