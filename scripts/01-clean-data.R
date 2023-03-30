b8dd4026a576599c4ccac50ef1f77442373c5c3c
Quinn Webber <46348512+qwebber@users.noreply.github.com>
2023-03-30 11:04:38 -0400

    Update 01-clean-data.R

--- scripts/01-clean-data.R
+++ scripts/01-clean-data.R
@@ -11,6 +11,11 @@ age <- read_csv("input/age-parasitism.csv")
 ## rename some column names
 age <- rename(age, relationship = `type of age~parasite relationship`)
 
+age$condensed_drivers_parasitism[age$other_drivers_parasitism == "MHC" |
+                                 age$other_drivers_parasitism == "hibernation_period" |
+                                 age$other_drivers_parasitism == "physiological"] <- "physiology"
+
+
 ## make minor changes to data 
 age$relationship[age$relationship == "?"] <- "unknown"
 
@@ -50,9 +55,11 @@ setDT(df1)[, count := sum(n), by = c("parasite_taxonomy", "host_taxonomy")]
 ## calculate proportion 
 df1$prop <- df1$n/df1$count
 
+setDT(df1)
+
 ## visualize (cut out viruses)
 ggplot(df1[parasite_taxonomy != "virus"]) +
-  geom_col(aes(y = prop, 
+  geom_col(aes(y = n, 
                x = relationship, 
                fill = host_taxonomy), 
            position = "dodge2") + 
@@ -60,14 +67,26 @@ ggplot(df1[parasite_taxonomy != "virus"]) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 
+## Color brewer website: https://colorbrewer2.org/#type=diverging&scheme=RdYlBu&n=5
 ## stacked bar plot
 ggplot(df1[parasite_taxonomy != "virus"]) +
   geom_col(aes(y = prop, 
                x = relationship, 
                fill = host_taxonomy), 
            position = "fill") + 
+  ylab("Proportion of articles") +
+  scale_fill_manual(values = c("red", "#2c7bb6" , "purple", "green", "pink"),
+                    name = "Host taxonomy") +
   facet_wrap(~parasite_taxonomy*age_measure) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        strip.background = element_rect(color = "black", 
+                                        fill = "white", 
+                                        size = 1),
+        strip.text = element_text(size = 10, 
+                                  color = "black"),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(), 
+        panel.border = element_rect(colour = "black", fill=NA, size = 1))
 
 ## number of relationships by host age (continuous age only)
 age %>%
